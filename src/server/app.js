264f8096bc798b9f4511f01a4ecefc92fedd3e8d
Oli <olli.kiseleva@gmail.com>
2020-04-26 14:41:48 +0000

    Connecting to the playlist DB in the Mongo DB Atlas, uri with credentials stored in the environment variable

--- src/server/app.js
+++ src/server/app.js
@@ -9,6 +9,21 @@ const address = process.env.IP || "0.0.0.0";
 
 router.use(express.static('dist'));
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = process.env.MONGODB_IWA_URL;
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+client.connect(err => {
+    if (err) {
+        console.error(err);
+        client.close();
+        return;
+    }
+    const collection = client.db("playlist").collection("playlist");
+    // perform actions on the collection object
+    client.close();
+});
+
 router.listen(port, address, function() {
   console.log("Server listening at", address + ":" + port);
 });
